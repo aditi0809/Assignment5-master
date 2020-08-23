30f591a7453ac944b1c846be2c0ae790e7a4649d
AditiKapoor56 <a502@my.centennialcollege.ca>
2020-08-23 19:10:53 -0400

    Add project files.

---
+++ Assignment5-master/Assignment5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment5
+{
+    static class Program
+    {
+        public static SplashForm splashForm;
+        public static StartForm startForm;
+        public static SelectForm selectForm;
+        public static Product selectedProduct;
+        public static ProductInfoForm productInfoForm;
+        public static OrderForm orderForm;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            splashForm = new SplashForm();
+            startForm = new StartForm();
+            selectForm = new SelectForm();
+            productInfoForm = new ProductInfoForm();
+            orderForm = new OrderForm();
+
+            Application.Run(splashForm);
+        }
+    }
+}
---
+++ Assignment5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ Name =  Arshdeep Singh 
+ Student Id no = 301108498
+ Assignment 5
+ Created Date = 20/08/2020
+ Modify Date  =  21/08/2020 
+ */
+
+namespace Assignment5
+{
+    static class Program
+    {
+        public static SplashForm splashForm;
+        public static StartForm startForm;
+        public static SelectForm selectForm;
+        public static Product selectedProduct;
+        public static ProductInfoForm productInfoForm;
+        public static OrderForm orderForm;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            splashForm = new SplashForm();
+            startForm = new StartForm();
+            selectForm = new SelectForm();
+            productInfoForm = new ProductInfoForm();
+            orderForm = new OrderForm();
+
+            Application.Run(splashForm);
+        }
+    }
+}
